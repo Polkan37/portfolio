1c76469bd1ead9584b16c2532f5dbdfdb30d4cfb
Нескоромна Маргарита <u1-775@office107.local>
2022-10-06 18:01:08 +0300

    refactor: form validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,8 +5,13 @@ const patterns = {
   phone: /[0-9]{3}[- ]?[0-9]{3}[- ]?[0-9]{4}/,
   email: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   message: /[\S\s]+[\S]+/,
-}
-let fieldValidaitonStatus = {name: false, phone: false, email: false, message: false }
+};
+let fieldValidaitonStatus = {
+  name: false,
+  phone: false,
+  email: false,
+  message: false,
+};
 
 const formReducer = (state, event) => {
   if (event.reset) {
@@ -29,70 +34,40 @@ function Form() {
   const handleSubmit = (event) => {
     event.preventDefault();
     setSubmitting(true);
-    document.getElementById('message-sent').style.transform = 'scale(1)';
-    console.log('formData', formData);
+    document.getElementById("message-sent").style.transform = "scale(1)";
     setTimeout(() => {
-      SendForm(formData, 'fetch');
+      SendForm(formData, "fetch");
       setSubmitting(false);
       setFormData({
         reset: true,
       });
-      fieldValidaitonStatus = {name: false, phone: false, email: false};
-      document.getElementById('form-button').classList.remove('active');
-      document.getElementById('name').style.removeProperty('border');
-      document.getElementById('phone').style.removeProperty('border');
-      document.getElementById('email').style.removeProperty('border');
-      document.getElementById('msg').style.removeProperty('border');
-      document.getElementById('message-sent').style.transform = 'scale(0)';
+      fieldValidaitonStatus = { name: false, phone: false, email: false };
+      for (const [key] of Object.entries(formData)) {
+        document.getElementById(key).style.removeProperty("border");
+      }
+      document.getElementById("form-button").classList.remove("active");
+      document.getElementById("message-sent").style.transform = "scale(0)";
     }, 1500);
   };
-  
+
   const handleChange = (event) => {
-    
-    switch(event.target.name) {
-      case 'name':
-        if(patterns.name.test(event.target.value)) {
-          document.getElementById('name').style.border = '1px solid #00856f';
-          fieldValidaitonStatus.name = true;
-        } else {
-          document.getElementById('name').style.border = '1px solid red';
-          fieldValidaitonStatus.name = false;
-        }
-        break;
-      case 'phone':
-        if(patterns.phone.test(event.target.value)) {
-          document.getElementById('phone').style.border = '1px solid #00856f';
-          fieldValidaitonStatus.phone = true;
-        } else {
-          document.getElementById('phone').style.border = '1px solid red';
-          fieldValidaitonStatus.phone = false;
-        }
-        break;
-      case 'email':
-        if(patterns.email.test(event.target.value)) {
-          document.getElementById('email').style.border = '1px solid #00856f';
-          fieldValidaitonStatus.email = true;
-        } else {
-          document.getElementById('email').style.border = '1px solid red';
-          fieldValidaitonStatus.email = false;
-        }
-        break;
-      case 'message':
-          if(patterns.message.test(event.target.value)) {
-            document.getElementById('msg').style.border = '1px solid #00856f';
-            fieldValidaitonStatus.message = true;
-          } else {
-            document.getElementById('msg').style.border = '1px solid red';
-            fieldValidaitonStatus.message = false;
-          }
-          break;
-      default:
-        break;
+    const title = event.target.name;
+    const value = event.target.value;
+    if (patterns[title].test(value)) {
+      document.getElementById([title]).style.border = "1px solid #00856f";
+      fieldValidaitonStatus[title] = true;
+    } else {
+      document.getElementById(title).style.border = "1px solid red";
+      fieldValidaitonStatus[title] = false;
     }
-    
-    const status = Object.values(fieldValidaitonStatus).every(item => item === true);
-    status ? document.getElementById('form-button').classList.add('active') : document.getElementById('form-button').classList.remove('active');
-    
+
+    const status = Object.values(fieldValidaitonStatus).every(
+      (item) => item === true
+    );
+    status
+      ? document.getElementById("form-button").classList.add("active")
+      : document.getElementById("form-button").classList.remove("active");
+
     setFormData({
       name: event.target.name,
       value: event.target.value,
@@ -116,7 +91,10 @@ function Form() {
             value={formData.name || ""}
             required
           />
-          <input type="tel" id="phone" name="phone"
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
             pattern="[0-9]{3}[- ]?[0-9]{3}[- ]?[0-9]{4}"
             placeholder="Телефон"
             onChange={handleChange}
@@ -133,7 +111,7 @@ function Form() {
             required
           />
           <textarea
-            id="msg"
+            id="message"
             name="message"
             placeholder="Сообщение..."
             onChange={handleChange}
@@ -145,8 +123,8 @@ function Form() {
           </button>
         </fieldset>
       </form>
-      <div className="modal-done" id='message-sent'>
-          <p>✓</p>
+      <div className="modal-done" id="message-sent">
+        <p>✓</p>
       </div>
     </div>
   );
