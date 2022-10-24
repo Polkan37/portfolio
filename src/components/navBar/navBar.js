7b5ad50d59d76ac518eda10a2cb44f48612e5289
Polkan37 <zaq2587413699@gmial.com>
2022-10-24 17:20:07 +0300

    fix: close burger when item was clicked. Feat: smooth scroll

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,29 +1,21 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-scroll";
 import "./navBar.css";
 import logo from "../../resources/img/logo.png";
-import { navItems}  from '../../constants/header.constants';
+import { navItems } from "../../constants/header.constants";
 const tabletWidth = 828;
 
-const list = navItems.map((item) => {
-  return (
-    <li className="nav__item" key={item.key}>
-      <a href={item.link} className="nav__link">
-        {item.name}
-      </a>
-    </li>
-  );
-});
-
 function getWindowSize() {
-    const {innerWidth, innerHeight} = window;
-    return {innerWidth, innerHeight};
+  const { innerWidth, innerHeight } = window;
+  return { innerWidth, innerHeight };
 }
 
 function NavBar() {
+  const ref = useRef();
   const [burger_button_class, setBurgerButtonClass] = useState("nav__button");
   const [burger_class, setBurgerClass] = useState("nav__button-line");
   const [menu_class, setMenuClass] = useState("nav__sidebar hidden");
-  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [windowSize, setWindowSize] = useState(getWindowSize());
 
   useEffect(() => {
@@ -31,47 +23,77 @@ function NavBar() {
       setWindowSize(getWindowSize());
     }
 
-    window.addEventListener('resize', handleWindowResize);
-    if(windowSize.innerWidth > tabletWidth){
-        setMenuClass("nav__items");
-        setBurgerButtonClass("nav__button hidden");
-      }
-      else {
-        setMenuClass("nav__sidebar hidden");
-        setBurgerButtonClass("nav__button");
-      }
+    window.addEventListener("resize", handleWindowResize);
+    if (windowSize.innerWidth > tabletWidth) {
+      setMenuClass("nav__items");
+      setBurgerButtonClass("nav__button hidden");
+    } else {
+      setMenuClass("nav__sidebar hidden");
+      setBurgerButtonClass("nav__button");
+    }
     return () => {
-      window.removeEventListener('resize', handleWindowResize);
+      window.removeEventListener("resize", handleWindowResize);
     };
-  },[windowSize.innerWidth]);
+  }, [windowSize.innerWidth]);
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (isMenuOpen && ref.current && !ref.current.contains(e.target)) {
+        setIsMenuOpen(false);
+        updateMenu();
+      }
+    };
 
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [isMenuOpen]);
 
   const updateMenu = () => {
-      if(!isMenuClicked){
-          setBurgerButtonClass("nav__button fixed-top-right");
-          setBurgerClass("nav__button-line clicked");
-          setMenuClass("nav__sidebar")
-      }
-      else {
-        setBurgerButtonClass("nav__button");
-        setBurgerClass("nav__button-line");
-        setMenuClass("nav__sidebar hidden")
-      }
-      setIsMenuClicked(!isMenuClicked);
-  }
+    if (!isMenuOpen) {
+      setBurgerButtonClass("nav__button fixed-top-right");
+      setBurgerClass("nav__button-line clicked");
+      setMenuClass("nav__sidebar");
+    } else {
+      setBurgerButtonClass("nav__button");
+      setBurgerClass("nav__button-line");
+      setMenuClass("nav__sidebar hidden");
+    }
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div className="nav container">
       <a href="#top" className="nav__logo">
         <img src={logo} alt="Dartovich site logo" />
       </a>
-      <nav className="nav__list">
+      <nav className="nav__list" ref={ref}>
         <div className={burger_button_class} onClick={updateMenu}>
           <div className={burger_class}></div>
           <div className={burger_class}></div>
           <div className={burger_class}></div>
         </div>
-        <ul className={menu_class}>{list}</ul>
+        <ul className={menu_class}>
+          {navItems.map((item) => {
+            return (
+              <li className="nav__item" key={item.key}>
+                <Link
+                  className="nav__link"
+                  to={item.link}
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  onClick={updateMenu}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </nav>
     </div>
   );
--- src/constants/header.constants.js
+++ src/constants/header.constants.js
@@ -11,10 +11,10 @@ export const mainInfo = {
   };
 
 export const navItems = [
-    { name: "About", link: "#about", key: 1 },
-    { name: "Portfolio", link: "#portfolio", key: 2 },
-    { name: "Price", link: "#price", key: 3 },
-    { name: "Contact me", link: "#contact", key: 4 },
+    { name: "About", link: "about", key: 1 },
+    { name: "Portfolio", link: "portfolio", key: 2 },
+    { name: "Price", link: "price", key: 3 },
+    { name: "Contact me", link: "contact", key: 4 },
 ];
 
 export const links = [
