e6592b698736cad9f2ce932d027cc61a621632d3
Polkan37 <zaq2587413699@gmial.com>
2022-10-26 12:12:33 +0300

    fix: min images in portfolio 4

--- src/components/PortfolioSection/PortfolioSection.js
+++ src/components/PortfolioSection/PortfolioSection.js
@@ -1,36 +1,39 @@
-import React, { useState} from "react";
-import Masonry from "../Masonry/Masonry";
+import React, { useState } from "react";
+import Masonry from "./Masonry";
 import "./portfolio.css";
-import {imageUrls} from '../../constants/portfolioImages';
-const minAmounOfImageInGallery = 7;
+import { imageUrls } from "../../constants/portfolioImages";
+const minAmounOfImageInGallery = 5;
 
 function PortfolioSection() {
   let [imgAmount, setImgAmount] = useState(minAmounOfImageInGallery);
-  const [display, setDisplay] = useState('block');
+  const [display, setDisplay] = useState("block");
 
   const showMore = () => {
-    if((imageUrls.length - imgAmount) > minAmounOfImageInGallery) {
-      imgAmount += minAmounOfImageInGallery
-     }
-     else {
-      imgAmount += (imageUrls.length - imgAmount);
+      imgAmount += imageUrls.length - imgAmount;
       setDisplay("none");
-     }
-    console.log(imgAmount);
-    return setImgAmount(imgAmount);
-  }
+      setImgAmount(imgAmount);
+  };
 
   return (
     <section className="portfolio">
       <div className="backdrop">
         <div className="horisontal-line">
-          <h2 className="portfolio__title title" id="portfolio">Portfolio</h2>
+          <h2 className="portfolio__title title" id="portfolio">
+            Portfolio
+          </h2>
         </div>
         <div className="portfolio__container container">
           <div className="portfolio__galery">
-            <Masonry imageUrls={imageUrls} gap="10" size={imgAmount}/>
+            <Masonry imageUrls={imageUrls} gap="10" size={imgAmount}/> 
           </div>
-          <button className="portfolio__button button" onClick={showMore} style={{display: display}}>Show more</button>
+
+          <button
+            className="portfolio__button button"
+            onClick={showMore}
+            style={{ display: display }}
+          >
+            Show more
+          </button>
         </div>
       </div>
     </section>
--- src/constants/portfolioImages.js
+++ src/constants/portfolioImages.js
@@ -12,130 +12,16 @@ import photo11 from '../resources/img/portfolio/photo-2.png';
 
 export const imageUrls = [
     { id: 1, url: photo1, width: 1, height: 1, alt: '1'},
-    { id: 2, url: photo2, width: 1, height: 1, alt: '2'}, 
-    { id: 3, url: photo3, width: 1, height: 1, alt: '3'},
-    { id: 4, url: photo4, width: 1, height: 1, alt: '4'}, 
-    { id: 5, url: photo5, width: 1, height: 1, alt: '5'}, 
-    { id: 6, url: photo6, width: 1, height: 1, alt: '6'}, 
-    { id: 7, url: photo7, width: 1, height: 1, alt: '7'}, 
-    { id: 8, url: photo8, width: 1, height: 1, alt: '8'}, 
+    { id: 2, url: photo10, width: 1, height: 1, alt: '3'},
+    { id: 3, url: photo2, width: 1, height: 1, alt: '2'}, 
+    { id: 4, url: photo7, width: 1, height: 1, alt: '9'},
+    { id: 5, url: photo4, width: 1, height: 1, alt: '4'}, 
+    { id: 6, url: photo2, width: 1, height: 1, alt: '2'}, 
+    { id: 7, url: photo5, width: 1, height: 1, alt: '5'}, 
+    { id: 8, url: photo6, width: 1, height: 1, alt: '6'}, 
     { id: 9, url: photo9, width: 1, height: 1, alt: '9'}, 
-    { id: 10, url: photo10, width: 1, height: 1, alt: '10'}, 
-    { id: 11, url: photo11, width: 1, height: 1, alt: '11'},
-    { id: 12, url: photo3, width: 1, height: 1, alt: '12'}, 
+    { id: 10, url: photo3, width: 1, height: 1, alt: '3'}, 
+    { id: 11, url: photo8, width: 1, height: 1, alt: '9'},
+    { id: 12, url: photo11, width: 1, height: 1, alt: '11'},
+    { id: 13, url: photo1, width: 1, height: 1, alt: '1'},
     ];
-
-export const IMAGES = [
-    {
-      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 174,
-      caption: "After Rain (Jeshu John - designerspics.com)"
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 320,
-      tags: [
-        { value: "Ocean", title: "Ocean" },
-        { value: "People", title: "People" }
-      ],
-      caption: "Boats (Jeshu John - designerspics.com)"
-    },
-  
-    {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 320,
-      caption: "After Rain (Jeshu John - designerspics.com)"
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212,
-      tags: [
-        { value: "Ocean", title: "Ocean" },
-        { value: "People", title: "People" }
-      ],
-      caption: "Boats (Jeshu John - designerspics.com)"
-    },
-  
-    {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 174,
-      caption: "After Rain (Jeshu John - designerspics.com)"
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212,
-      tags: [
-        { value: "Ocean", title: "Ocean" },
-        { value: "People", title: "People" }
-      ],
-      caption: "Boats (Jeshu John - designerspics.com)"
-    },
-  
-    {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 174,
-      caption: "After Rain (Jeshu John - designerspics.com)"
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-      thumbnail:
-        "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212,
-      tags: [
-        { value: "Ocean", title: "Ocean" },
-        { value: "People", title: "People" }
-      ],
-      caption: "Boats (Jeshu John - designerspics.com)"
-    },
-  
-    {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail:
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 720,
-      thumbnailHeight: 212
-    }
-  ];
